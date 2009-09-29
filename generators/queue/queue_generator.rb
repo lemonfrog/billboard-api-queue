e950dd1eea2888f816312f4d9c1b9493d410b244
marcoribi <marco.ribi@simplificator.com>
2009-09-29 15:53:09 +0200

    add namespace for queue model

--- generators/queue/queue_generator.rb
+++ generators/queue/queue_generator.rb
@@ -2,7 +2,7 @@ class QueueGenerator < Rails::Generator::Base
 
   def manifest    
     record do |m|
-      m.file "models/order_queue.rb", "app/models/order_queue.rb"
+      m.file "models/order_queue.rb", "app/models/billboard_api/order_queue.rb"
       m.migration_template "migrate/create_order_queue.rb", "db/migrate", :migration_file_name => custom_file_name
       m.readme "USAGE"
     end
