63ba14ba22c5a0155f4121133061fdaae62a4773
Tino Wendland <tinowendland@gmail.com>
2013-06-25 10:38:02 +0200

    removed output statements if nothing is in queue

--- lib/billboard-api-queue/tasks.rb
+++ lib/billboard-api-queue/tasks.rb
@@ -1,8 +1,6 @@
 namespace :billboard do
   desc "Process local Order-Queue"
   task :process_queue => [:environment] do
-    puts "Start process queue entries"
-    
     BillboardApi::OrderQueue.all.each do |entry|
       puts "Procesing #{entry}"
       customer = entry.user
@@ -28,7 +26,5 @@ namespace :billboard do
         end
       end
     end
-    
-    puts "Queue entries were processed"
   end
 end
